fd7f641047755c23459637ab8ede8db87662618b
dvir avrahami <dviravr@gmail.com>
2020-12-22 23:38:25 +0200

    upload files

--- client.c
+++ client.c
@@ -15,74 +15,94 @@
 #include <unistd.h>
 
 
-
+#define MB 1024
 #define SERVER_PORT 5060
-#define SERVER_IP_ADDRESS "172.17.17.23"
+#define SERVER_IP_ADDRESS "127.0.0.1"
 
 int main()
 {
     
      int sock = socket(AF_INET, SOCK_STREAM, 0);
-
+          
      if(sock == -1)
-    {
-        printf("Could not create socket : %d" 	,errno );
-    }
-    // "sockaddr_in" is the "derived" from sockaddr structure
-    // used for IPv4 communication. For IPv6, use sockaddr_in6
-    //
-    struct sockaddr_in serverAddress;
-    memset(&serverAddress, 0, sizeof(serverAddress));
+     {
+          printf("Could not create socket : %d" 	,errno );
+          return 1;
+     }
+     // "sockaddr_in" is the "derived" from sockaddr structure
+     // used for IPv4 communication. For IPv6, use sockaddr_in6
+     //
+     struct sockaddr_in serverAddress;
+     memset(&serverAddress, 0, sizeof(serverAddress));
 
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(SERVER_PORT);
-	int rval = inet_pton(AF_INET, (const char*)SERVER_IP_ADDRESS, &serverAddress.sin_addr);
-	if (rval <= 0)
-	{
-		printf("inet_pton() failed");
-		return -1;
-	}
+     serverAddress.sin_family = AF_INET;
+     serverAddress.sin_port = htons(SERVER_PORT);
+     int rval = inet_pton(AF_INET, (const char*)SERVER_IP_ADDRESS, &serverAddress.sin_addr);
+     if (rval <= 0)
+     {
+          printf("inet_pton() failed");
+          return -1;
+     }
 
      // Make a connection to the server with socket SendingSocket.
 
      if (connect(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) == -1)
      {
-	   printf("connect() failed with error code : %d" ,errno );
+     printf("connect() failed with error code : %d" ,errno );
      }
-
      
      printf("connected to server\n");
 
-     // Sends some data to server
-     char message[] = "Good morning, Vietnam\n";
-     int messageLen = strlen(message) + 1;
+     char* fileName = "1mb.txt";
+	FILE *fp;
+	fp = fopen(fileName, "r");
 
-     int bytesSent = send(sock, message, messageLen, 0);
+	char file[MB]; 
+	int n; 
+	float t;
+     int messageLen = strlen(file) + 1;
+     int i = 0;
 
-     if (-1 == bytesSent)
+     while (i++ < 5)
      {
-	printf("send() failed with error code : %d" ,errno);
-     }
-     else if (0 == bytesSent)
-     {
-	printf("peer has closed the TCP connection prior to send().\n");
-     }
-     else if (messageLen > bytesSent)
-     {
-	printf("sent only %d bytes from the required %d.\n", messageLen, bytesSent);
-     }
-     else 
-     {
-	printf("message was successfully sent .\n");
-     }
+          while(fgets(file, MB, fp) != NULL);
+          int bytesSent = write(sock, file, sizeof(file));
+          if (bytesSent == -1) 
+          {
+               perror("Error in sending file.");
+               return;
+          }
+          else if (0 == bytesSent)
+          {
+               printf("peer has closed the TCP connection prior to send().\n");
+          }
+          else if (messageLen > bytesSent)
+          {
+               printf("sent only %d bytes from the required %d.\n", messageLen, bytesSent);
+          }
+          else 
+          {
+               printf("message was successfully sent .\n");
+          }
+          printf("bytes send: %d .\n", bytesSent);
 
-	 sleep(3);
+          bzero(file, MB);
+          read(sock, &n,sizeof(int));
+          //printf("the server got %d bytes", n);
+     }
+     
+
+	
+          //printf("it's took %f seconds to get the file\n", t);
+  	//}
+
+     //sleep(3);
 
 
 // TODO: All open clientSocket descriptors should be kept
-    // in some container and closed as well.
+// in some container and closed as well.
 
-        close(sock);
+     close(sock);
 
      return 0;
 }
--- server.c
+++ server.c
@@ -12,7 +12,8 @@
 #include <string.h>
 #include <errno.h>
 #include <signal.h>
-
+#include <time.h> 
+#define MB 1024
 #define SERVER_PORT 5060  //The port that the server listens
   
 int main()
@@ -25,6 +26,7 @@ int main()
     if((listeningSocket = socket(AF_INET , SOCK_STREAM , 0 )) == -1)
     {
         printf("Could not create listening socket : %d" ,errno);
+        return 1;
     }
 
 	// Reuse the address if the server socket on was closed
@@ -57,7 +59,7 @@ int main()
     if (listen(listeningSocket, 500) == -1) //500 is a Maximum size of queue connection requests
 											//number of concurrent connections 
     {
-	printf("listen() failed with error code : %d" ,errno);
+	    printf("listen() failed with error code : %d" ,errno);
         return -1;
     }
        
@@ -74,34 +76,42 @@ int main()
         int clientSocket = accept(listeningSocket, (struct sockaddr *)&clientAddress, &clientAddressLen);
     	if (clientSocket == -1)
     	{
-           printf("listen failed with error code : %d",errno);
+            printf("listen failed with error code : %d",errno);
            	   // TODO: close the sockets
-        return -1;
+            return -1;
         }
         printf("A new client connection accepted\n");
   
     	//Reply to client
-    	char message[] = "Welcome to our TCP-server\n";
-        int messageLen = strlen(message) + 1;
+    	//char message[] = "Welcome to our TCP-server\n";
+//        int messageLen = strlen(message) + 1;
 
-    	int bytesSent = send(clientSocket, message, messageLen, 0);
-		if (-1 == bytesSent)
-		{
-			printf("send() failed with error code : %d" ,errno);
+
+        int n, i = 0;
+        FILE *fp;
+        char *filename = "recv.txt";
+        char buffer[MB];
+        clock_t t;
+
+        fp = fopen(filename, "w");
+        t = clock();
+        while (1) 
+        {
+            n = read(clientSocket, buffer, MB);
+            i += n;
+            if (n <= 0)
+            {
+                break;
+                return;
+            }
+            fprintf(fp, "%s", buffer);
+            bzero(buffer, MB);
         }
-        else if (0 == bytesSent)
-		{
-		   printf("peer has closed the TCP connection prior to send().\n");
-		}
-		else if (messageLen > bytesSent)
-		{
-		   printf("sent only %d bytes from the required %d.\n", messageLen, bytesSent);
-		}
-		else 
-		{
-		   printf("message was successfully sent .\n");
-		}
+        t = clock() - t;
+        float time = ((float) t) / CLOCKS_PER_SEC;
+        printf("it's took %f seconds to get the file, recive %d bytes\n", time, i);
 
+        write(clientSocket, &i, sizeof(int));    
     }
   
     // TODO: All open clientSocket descriptors should be kept
